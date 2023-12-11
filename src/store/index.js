4228bb31e9573d0be048781c4c097540b70359e8
WilsonSan5 <wilson.san57@gmail.com>
2023-12-11 16:10:42 +0100

    changed fusion mode for character image

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ export default createStore({
         return {
             // Default URL
             defaultURL: 'https://127.0.0.1:8000',
+            //defaultURL: 'https://intranet.whowins.fun',
             // Home
             // Don't fetch if already fetched
             allfights: [],
