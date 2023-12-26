1b333f96c0863885deea9d005853dadfca803995
WilsonSan5 <wilson.san57@gmail.com>
2023-12-26 20:59:05 +0100

    stylisize ranking page

--- src/views/RankingView.vue
+++ src/views/RankingView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import axios from 'axios'
 import { ref, computed } from 'vue'
+import store from '../store'
+const defaultURL = store.state.defaultURL
 const ranking = ref()
 const strength_filter = ref()
 async function getRanking() {
@@ -15,44 +17,114 @@ async function getRanking() {
   }
 }
 getRanking()
-strength_filter.value = 4
+
+strength_filter.value = 5
 const ranking_by_strength = computed(() => {
   return ranking.value.filter((f) => f.strength === strength_filter.value)
 })
 </script>
 <template>
-  <h1>Leaderboard</h1>
-  <div class="tabSelector">
-    <div class="tab" :class="{ active: strength_filter == 5 }" @click="strength_filter = 5">
-      <p>5</p>
-      <img src="../assets/icon/star.svg" alt="star to indicate strength" />
-    </div>
-    <div class="tab" :class="{ active: strength_filter == 4 }" @click="strength_filter = 4">
-      <p>4</p>
-      <img src="../assets/icon/star.svg" alt="star to indicate strength" />
-    </div>
-    <div class="tab" :class="{ active: strength_filter == 3 }" @click="strength_filter = 3">
-      <p>3</p>
-      <img src="../assets/icon/star.svg" alt="star to indicate strength" />
-    </div>
-    <div class="tab" :class="{ active: strength_filter == 2 }" @click="strength_filter = 2">
+  <div class="wrapper">
+    <h1>Leaderboard</h1>
+    <p>
+      All fighters are categorized by strength from 3 to 5 stars. As they can only fight characters
+      with the same strength, leaderboard is separated.
+    </p>
+    <div class="tabSelector">
+      <div class="tab" :class="{ active: strength_filter == 5 }" @click="strength_filter = 5">
+        <p>5</p>
+        <img src="../assets/icon/star.svg" alt="star to indicate strength" />
+      </div>
+      <div class="tab" :class="{ active: strength_filter == 4 }" @click="strength_filter = 4">
+        <p>4</p>
+        <img src="../assets/icon/star.svg" alt="star to indicate strength" />
+      </div>
+      <div class="tab" :class="{ active: strength_filter == 3 }" @click="strength_filter = 3">
+        <p>3</p>
+        <img src="../assets/icon/star.svg" alt="star to indicate strength" />
+      </div>
+      <!-- <div class="tab" :class="{ active: strength_filter == 2 }" @click="strength_filter = 2">
       <p>2</p>
       <img src="../assets/icon/star.svg" alt="star to indicate strength" />
+    </div> -->
     </div>
-  </div>
-  <ol>
-    <li v-for="fighter in ranking_by_strength" :key="fighter.name">
-      <div class="flex">
-        <div>{{ fighter.name }}</div>
-        <div>{{ fighter.percentage }} %</div>
+    <!-- FIRST PLACE -->
+    <div class="podium">
+      <img :src="defaultURL + ranking_by_strength[0].image" />
+      <h2>{{ ranking_by_strength[0].name }}</h2>
+      <p>winrate <br />{{ ranking_by_strength[0].percentage }} %</p>
+      <div class="medal">1</div>
+    </div>
+    <div class="flex second-row">
+      <!-- SECOND PLACE -->
+      <div class="podium second-place">
+        <img :src="defaultURL + ranking_by_strength[1].image" />
+        <h2>{{ ranking_by_strength[1].name }}</h2>
+        <p>winrate <br />{{ ranking_by_strength[0].percentage }} %</p>
+        <div class="medal">2</div>
       </div>
-    </li>
-  </ol>
+      <!-- THIRD PLACE -->
+      <div class="podium third-place">
+        <img :src="defaultURL + ranking_by_strength[2].image" />
+        <h2>{{ ranking_by_strength[2].name }}</h2>
+        <p>winrate <br />{{ ranking_by_strength[0].percentage }} %</p>
+        <div class="medal">3</div>
+      </div>
+    </div>
+    <ol start="4">
+      <li v-for="fighter in ranking_by_strength.slice(3)" :key="fighter.name">
+        <div class="flex">
+          <div>{{ fighter.name }}</div>
+          <div>{{ fighter.percentage }} %</div>
+        </div>
+      </li>
+    </ol>
+  </div>
 </template>
 <style scoped>
+.wrapper {
+  padding: 0 1.7em;
+  margin: auto;
+  max-width: 800px;
+}
 h1 {
+  padding: 1rem 0;
   text-align: center;
   margin-bottom: 1em;
+}
+.tabSelector {
+  margin: 1em auto;
+  padding: 5px 5px;
+  width: 90%;
+  height: 45px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #373737;
+  border-radius: 35px;
+}
+.tab {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  border-radius: 35px;
+  padding: 0 1em;
+  cursor: pointer;
+  /* Flex basis  */
+  flex-basis: 33%;
+}
+.tab img {
+  margin-top: -2px;
+  height: 20px;
+}
+.tab p {
+  font-size: 20px;
+}
+.active {
+  color: black;
+  background-color: #dcdcdc;
 }
 ol {
   width: 90%;
@@ -69,37 +141,74 @@ li {
   justify-content: space-between;
 }
 
-.tabSelector {
+/* Podium  */
+.podium {
   margin: auto;
-  padding: 5px 5px;
-  width: 90%;
-  height: 45px;
+  width: 170px;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #373737;
-  border-radius: 35px;
-}
-.tab {
-  height: 100%;
-  display: flex;
-  flex-basis: 25%;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 5px;
-  border-radius: 35px;
-  padding: 0 1em;
-  cursor: pointer;
+  overflow: hidden;
 }
-.tab img {
-  margin-top: -2px;
-  height: 20px;
+.podium img {
+  position: relative;
+  mix-blend-mode: screen;
+  border: 3px solid white;
+  border-radius: 50%;
+  aspect-ratio: 1/1;
+  width: 100%;
+  object-fit: cover;
 }
-.tab p {
+.podium h2 {
   font-size: 20px;
 }
-.active {
-  color: black;
-  background-color: rgb(177, 177, 177);
+.podium > h2,
+p {
+  text-align: center;
+}
+.medal {
+  /* shape */
+  border-radius: 50px;
+  aspect-ratio: 1/1;
+  width: 50px;
+  height: 50px;
+  /* Position */
+  position: relative;
+  bottom: 130px;
+  left: 60px;
+  /* Font */
+  text-align: center;
+  font-family: var(--font-logo);
+  font-size: 32px;
+  /* background */
+  background: rgb(255, 255, 36);
+  background: linear-gradient(180deg, rgb(255, 220, 42) 28%, rgb(200, 147, 0) 73%);
+  padding: 1px 4px;
+}
+
+/* Second and third place */
+.second-row {
+  margin-top: -70px;
+}
+.second-row .podium img {
+  width: 80%;
+}
+.second-place .medal {
+  width: 40px;
+  height: 40px;
+  font-size: 25px;
+  background: rgb(63, 124, 158);
+  background: linear-gradient(0deg, rgba(63, 124, 158, 1) 28%, rgba(69, 173, 232, 1) 73%);
+}
+.third-place .medal {
+  width: 40px;
+  height: 40px;
+  font-size: 25px;
+  background: rgb(138, 86, 48);
+  background: linear-gradient(0deg, rgba(138, 86, 48, 1) 28%, rgba(226, 143, 44, 1) 73%);
+}
+.second-row .podium img {
+  width: 80%;
 }
 </style>
